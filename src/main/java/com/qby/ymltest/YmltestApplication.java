30c3ae56ea090d499648d77064867ab2aa71c2a5
cleanerq <qbyun130@163.com>
2020-06-02 13:37:36 +0800

    ddd

---
+++ src/main/java/com/qby/ymltest/YmltestApplication.java
@@ -0,0 +1,13 @@
+package com.qby.ymltest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YmltestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YmltestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/qby/ymltest/YmltestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qby.ymltest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YmltestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
